1253416219cc1462cd3a90387e4b019eda644f1b
sonxuz <sonxudakrong@gmail.com>
2024-12-06 09:01:00 +0700

    upload so 2

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         xin chào thé giới sau tận thế <br></br>
+         <code>navar/bay.js</code>
         </p>
         <a
           className="App-link"
@@ -17,6 +21,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <button>click me</button>
+        </div>
       </header>
     </div>
   );
